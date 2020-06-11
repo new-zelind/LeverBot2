b90231635be96469175c99b0b4f4d26a18f87cee
new-zelind <zlin1055@gmail.com>
2020-06-11 01:53:27 -0400

    added machine identification command

--- src/cmd/index.ts
+++ src/cmd/index.ts
@@ -14,6 +14,7 @@ import "./contact";
 import "./gpa"; 
 import "./help";
 import "./lock";
+import "./machine";
 import "./map";
 import "./ping";
 import "./rm";
---
+++ src/cmd/machine.ts
@@ -0,0 +1,27 @@
+import Command, {Permissions} from "../lib/command";
+import {Message, TextChannel} from "discord.js";
+import * as os from "os";
+import {code} from "../lib/util";
+
+export default Command({
+    names: ["machine"],
+
+    documentation:{
+        description: "Lists the machine that ByrnesBot is running on.",
+        group: "DEV",
+        usage: "machine"
+    },
+
+    check: Permissions.owner,
+
+    exec(message: Message){
+        const {username} = os.userInfo();
+        const machine = os.hostname();
+        const type = os.type();
+        const arch = os.arch();
+
+        return message.channel.send(
+            code(`${username}@${machine}: ${type} ${arch}`)
+        );
+    }
+});

3341c7b3213395a7a87da9a346f70d9c6a5c1dae
new-zelind <zlin1055@gmail.com>
2020-06-14 19:55:30 -0400

    added easter eggs

--- src/passive/easterEggs.ts
+++ src/passive/easterEggs.ts
@@ -1,8 +1,9 @@
 import {addMessageHandler} from "../lib/message";
 import {client} from "../client";
+import { Message } from "discord.js";
 
 //don't ping the bot
-addMessageHandler((message) => {
+addMessageHandler((message:Message) => {
     if(!client.user || !message.mentions.users.has(client.user.id)) return false;
 
     const pingEmote = client.emojis.cache.find((emoji) => emoji.name === "ping");
@@ -10,4 +11,34 @@ addMessageHandler((message) => {
 
     message.react(pingEmote);
     return true;
+});
+
+const fuckYou:string[] = [
+    "Fuck you too.",
+    "No, but thank you for the offer.",
+    "Flatter someone else who's 3D.",
+    "What did I do to you?",
+    "Not cool.",
+    "Fuck you!",
+    "I'm flattered.",
+    "Here, you might need one of these:\nhttps://cm.maxient.com/reportingform.php?ClemsonUniv&layout_id=1",
+    "...",
+    "If I could feel emotion, I would almost certainly be confused right now."
+];
+
+//react to "fuck you"
+addMessageHandler((message:Message) => {
+    if(!client.user || !message.mentions.users.has(client.user.id)) return false;
+    if(!message.content.includes("fuck you")) return false;
+
+    message.channel.send(
+        `${fuckYou[Math.floor(Math.random() * (fuckYou.length - 1))]}`
+    );
+});
+
+//creeper, aww man
+addMessageHandler((message:Message) => {
+    if(!message.content.includes("creeper")) return false;
+
+    message.channel.send("Aww man");
 });

f3c9b2cb1d6317854e202d4276be3e2418d245ee
new-zelind <zlin1055@gmail.com>
2020-06-14 17:11:10 -0400

    redo compliance

--- src/cmd/compliance.ts
+++ src/cmd/compliance.ts
@@ -1,5 +1,7 @@
 import Command, {Permissions} from "../lib/command";
 import {timeoutCounts} from "../lib/timeout";
+import {makeEmbed} from "../lib/util";
+import {client} from "../client";
 
 export default Command({
     names: ["compliance"],
@@ -18,8 +20,17 @@ export default Command({
 
         //if the user doesn't exist, then they haven't been timed out.
         if(numCitations == null) numCitations = 0;
-        return message.channel.send(
-            `You have recieved **${numCitations}** citations.\nMaintain your compliance.\n_Glory to Byrnes/Lever._`
-        );
+        const embed = makeEmbed(message)
+            .setColor("#B91A1A")
+            .setTitle(`COMPLIANCE CHECK`)
+            .setThumbnail(client.user.displayAvatarURL());
+
+        let description = `Greetings, ${message.author.username}#${message.author.discriminator}.`;
+        description += `\nYou have recieved **${numCitations}** citations.`;
+        description += `\nMaintain your compliance.\n_Glory to Byrnes/Lever._`
+
+        embed.setDescription(description);
+
+        return message.channel.send(embed);
     }
 });
